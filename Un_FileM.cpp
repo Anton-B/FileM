7ff2701b3ae0361af74ef5577f271d5a3a8d1d21
Anton-B <anton4381@gmail.com>
2014-07-10 00:22:02 +0300

    FileM

--- Un_FileM.cpp
+++ Un_FileM.cpp
@@ -13,9 +13,18 @@ TFr_Main *Fr_Main;
 __fastcall TFr_Main::TFr_Main(TComponent* Owner)
         : TForm(Owner)
 {
-  LPath=new TStringList;
   LDir=new TStringList;
   LFile=new TStringList;
+  DiskList(Owner);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TFr_Main::DiskList(TObject *Sender)
+{
+  Lv1->Clear();
+  LDir->Clear();
+  LDir->Clear();
+  path="";
   int BufferSize = GetLogicalDriveStrings(0, NULL);
   char *Buffer = new char[BufferSize];
   TStringList *DiskList = new TStringList;
@@ -35,50 +44,25 @@ __fastcall TFr_Main::TFr_Main(TComponent* Owner)
 }
 //---------------------------------------------------------------------------
 
-void __fastcall TFr_Main::Button1Click(TObject *Sender)
-{
-  //ListFiles("D:\\",Memo1->Lines);
-  /*const char Names[6][2][10] =
-   {{"Rubble","Barny"},
-    {"Michael", "Johnson"},
-    {"Bunny", "Bugs"},
-    {"Silver", "HiHo"},
-    {"Simpson", "Bart"},
-    {"Squirrel", "Rockey"}};
- 
-  TListColumn  *NewColumn;
-  TListItem  *ListItem;
-  TListView   *ListView = new TListView(this);
- 
-  ListView->Parent = this;
-  ListView->Align = alClient;
-  ListView->ViewStyle = vsReport;
-  NewColumn = ListView->Columns->Add();
-  NewColumn->Caption = "Last";
-
-  NewColumn = ListView->Columns->Add();
-  NewColumn->Caption = "First";
-  for (int i = 0; i < 6; i++)
-  {
-    ListItem = ListView->Items->Add();
-    ListItem->Caption = Names[i][0];
-    ListItem->SubItems->Add(Names[i][1]);
-  }*/
-
-
-}
-//---------------------------------------------------------------------------
 
 void __fastcall TFr_Main::Lv1DblClick(TObject *Sender)
 {
+  if ((fl==-1)&&(((TListView*)Sender)->Selected->Caption=="<--"))
+  {
+    DiskList(Owner);
+    fl=0;
+    return;
+  }
   if (((TListView*)Sender)->Selected->Caption=="<--")
     path+="..\\";
   else
   {
-  if (fl==0)
-    path=((TListView*)Sender)->Selected->Caption;
-  else
-    path+=((TListView*)Sender)->Selected->Caption+"\\";
+    if (fl==0)
+    {
+      path=((TListView*)Sender)->Selected->Caption;
+    }
+    else
+      path+=((TListView*)Sender)->Selected->Caption+"\\";
   }
   fl=1;
   cD=0;
@@ -113,6 +97,15 @@ void __fastcall TFr_Main::Lv1DblClick(TObject *Sender)
   Lv1->Clear();
   for (int i=0;i<cD;i++)
   {
+    if ((i==0)&&(LDir->Strings[i]!="<--"))
+    {
+      //ShowMessage(LDir->Strings[i]);
+      ListItem = Lv1->Items->Add();
+      ListItem->Caption = "<--";
+      ListItem->ImageIndex=0;
+      i=1;
+      fl=-1;
+    }
     ListItem = Lv1->Items->Add();
     ListItem->Caption = LDir->Strings[i];
     ListItem->ImageIndex=0;
@@ -123,10 +116,36 @@ void __fastcall TFr_Main::Lv1DblClick(TObject *Sender)
     ListItem->Caption = LFile->Strings[i];
     ListItem->ImageIndex=-1;
   }
-  Application->ProcessMessages();
   LDir->Clear();
   LFile->Clear();
+  Application->ProcessMessages();
 }
 //---------------------------------------------------------------------------
 
+//ListFiles("D:\\",Memo1->Lines);
+  /*const char Names[6][2][10] =
+   {{"Rubble","Barny"},
+    {"Michael", "Johnson"},
+    {"Bunny", "Bugs"},
+    {"Silver", "HiHo"},
+    {"Simpson", "Bart"},
+    {"Squirrel", "Rockey"}};
+ 
+  TListColumn  *NewColumn;
+  TListItem  *ListItem;
+  TListView   *ListView = new TListView(this);
+ 
+  ListView->Parent = this;
+  ListView->Align = alClient;
+  ListView->ViewStyle = vsReport;
+  NewColumn = ListView->Columns->Add();
+  NewColumn->Caption = "Last";
 
+  NewColumn = ListView->Columns->Add();
+  NewColumn->Caption = "First";
+  for (int i = 0; i < 6; i++)
+  {
+    ListItem = ListView->Items->Add();
+    ListItem->Caption = Names[i][0];
+    ListItem->SubItems->Add(Names[i][1]);
+  }*/
--- Un_FileM.dfm
+++ Un_FileM.dfm
@@ -32,15 +32,6 @@ object Fr_Main: TFr_Main
     ViewStyle = vsReport
     OnDblClick = Lv1DblClick
   end
-  object Button1: TButton
-    Left = 920
-    Top = 304
-    Width = 75
-    Height = 65
-    Caption = 'Button1'
-    TabOrder = 1
-    OnClick = Button1Click
-  end
   object ImgL1: TImageList
     Top = 656
     Bitmap = {
